5e93ce9472be52d2761fd83e2004cfb7cea135cb
rayhankhan <khanrayhanali@gmail.com>
2022-11-15 13:07:31 -0600

    Register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,12 +1,38 @@
 import React from "react";
+import { registerUser } from "../api-adapter";
 
 const Register = () => {
+  // const [register, setRegister] = useState({
+  //   username: '',
+  //   password: ''
+  // });
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const username = event.target[0].value;
+    const password = event.target[1].value;
+    const registeredUser = await registerUser(username, password);
+    const token = registeredUser.token;
+    const message = registeredUser.message;
+    localStorage.removeItem("username");
+    localStorage.setItem("username", username);
+    localStorage.removeItem("token");
+    localStorage.setItem("token", token);
+    if (token.length) {
+
+    }
+  }
+
+// If time: display ERROR or CONFIRMATION on client side.
 
   return (
     <div>
     <div>I am Register</div>
-    <form>
-      
+    <form className="form" onSubmit={handleSubmit}>
+      <label className="formLabel">Username</label>
+      <input type="text" id='username' required></input>
+      <label className="formLabel">Password</label>
+      <input type='password' id='password' required></input>
+      <button type='submit'>Register</button>
     </form>
     </div>
   )
