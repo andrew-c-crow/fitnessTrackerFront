ccf29e8fb23bd44c8dfce5e5fb1a514c564f24a9
rayhankhan <khanrayhanali@gmail.com>
2022-11-16 13:03:35 -0600

    SeeActivities + Back functionality

--- src/components/SeeActivities.jsx
+++ src/components/SeeActivities.jsx
@@ -1,23 +1,37 @@
 import React from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 const SeeActivities = (props) => {
-  const {id} = useParams()
-  const filteredActivities= props.routineData.filter((element) => {
-    console.log(element.id)
-    if (element.id == id) {
+  const { routineid } = useParams();
+  const filteredActivities = props.routineData.filter((element) => {
+    if (element.id == routineid) return true;
+  });
 
-      return true
-    }
-  })
   return filteredActivities ? (
-    <div key={id}>
+    <div key={routineid}>
+      <Link to="/routines">
+        <button>Back</button>
+      </Link>
+      <h3>Selected Routine:</h3>
+      <h1>{filteredActivities[0].name}</h1>
       <div>
-        <h3>Name: {filteredActivities.name}</h3>
+        <h3>
+          {filteredActivities[0].activities.map((activity, index) => {
+            return (
+              <div key={index} className="singleActivity">
+                <div className="activityName">{activity.name}</div>
+                <div className="activityDesc">{activity.description}</div>
+                <div className="activityCount">Count: {activity.count}</div>
+                <div className="activityDuration">
+                  Duration: {activity.duration}
+                </div>
+              </div>
+            );
+          })}
+        </h3>
       </div>
     </div>
-  ) : null
+  ) : null;
+};
 
-}
-
-export default SeeActivities
+export default SeeActivities;
